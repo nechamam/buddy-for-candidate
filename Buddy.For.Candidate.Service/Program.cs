124c145d8957edf0e4e062a5be4d893f7d8a7d39
Dina Yakobovich <diyakobo@microsoft.com>
2024-09-15 22:35:19 +0300

    init project with Hello World and tasks

--- Buddy.For.Candidate.Service/Program.cs
+++ Buddy.For.Candidate.Service/Program.cs
@@ -1,25 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+var tasks = new List<TaskItem>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    new TaskItem(1, "Task 1", false),
+    new TaskItem(2, "Task 2", true),
+    new TaskItem(3, "Task 3", false)
+};
 
-app.UseHttpsRedirection();
+app.MapGet("/", () => "Hello World!");
 
-app.UseAuthorization();
-
-app.MapControllers();
-
+// GET endpoint with parameter in URL
+app.MapGet("/tasks/{id}", (int id) =>
+{
+    var task = tasks.FirstOrDefault(t => t.Id == id);
+    return task is not null ? Results.Ok(task) : Results.NotFound();
+});
 app.Run();
+public record TaskItem(int Id, string Name, bool IsComplete);
